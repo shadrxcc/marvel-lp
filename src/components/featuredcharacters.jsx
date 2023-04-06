6e98765eb263205a3c6a6d58cf7ae0603f3fac95
shadrach akaade <akaadesm@gmail.com>
2023-04-06 13:04:14 +0100

    remove carousel

--- src/components/featuredcharacters.jsx
+++ src/components/featuredcharacters.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { featChar } from "./images";
-// import Slider from "./swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+import Slider from "react-slick";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 // Import Swiper styles
 import "swiper/css";
@@ -12,39 +14,12 @@ const Featuredcharacters = () => {
   return (
     <>
       <div id="character-main">
+        
         <div className="character-container">
-          <Swiper
-            slidesPerView={1}
-            pagination={{
-              clickable: true,
-            }}
-            breakpoints={{
-              "@0.00": {
-                slidesPerView: 2,
-      
-              },
-              "@0.75": {
-                slidesPerView: 4,
-           
-              },
-              "@1.00": {
-                slidesPerView: 3,
-             
-              },
-              "@1.50": {
-                slidesPerView: 5,
-               
-              },
-            }}
-            modules={[Pagination]}
-            className="mySwiper"
-          >
           {featChar.map((character, id) => {
             const { char, heroname, name } = character;
             return (
               <>
-                <SwiperSlide>
-                  {" "}
                   <div className="relative" id="character" key={id}>
                     <img className="w-[16em]" src={char} />
                     <div className="character-detail">
@@ -52,15 +27,12 @@ const Featuredcharacters = () => {
                       <span>{name}</span>
                     </div>
                   </div>
-                </SwiperSlide>
               </>
             );
           })}
-        </Swiper></div>
+       </div>
       </div>
-      {/* <div style={{ background: "black" }}>
-        <Slider />
-      </div> */}
+    
     </>
   );
 };
